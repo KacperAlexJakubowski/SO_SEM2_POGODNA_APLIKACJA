ed6b66564466448d3067d2a0a9b3c1728ccde98e
Kacper Jakubowski <kacper.jakubowski.my@gmail.com>
2026-05-08 07:55:32 +0200

    getopts loop implementation including config file data fetch and update

--- main.sh
+++ main.sh
@@ -12,3 +12,44 @@
 # Generative AI statement (keep ONE line below, delete the others):
 # * I did NOT use GenAI tools while developing this code.
 
+VERSION="1.0"
+CONFIG_FILE="./config.config"
+
+if [ ! -f "$CONFIG_FILE" ]; then
+    echo "Nie znaleziono pliku konfiugracyjnego niezbędnego do prawidłowego działania aplikacji." >&2
+    exit 1
+fi
+source "$CONFIG_FILE"
+
+UPDATE_WEATHER=false
+while getopts ":hvlu" opt; do
+    case ${opt} in
+        h )
+            echo "Krótka pomoc"
+            exit 0
+            ;;
+        v )
+            echo "Version: $VERSION"
+            exit 0
+            ;;
+        l )
+            read -p "Nazwa nowej lokalizacji: " LOC
+            CONFIG_LOCATION=${LOC// /+}
+            sed -i "s#^CONFIG_LOCATION=.*#CONFIG_LOCATION=\"$LOC\"#" "$CONFIG_FILE"
+            echo "Zapisano nową lokalizację."
+            ;;
+        u )
+            UPDATE_WEATHER=true
+            ;;
+        \? )
+            echo "Nieprawidłowa opcja: -$OPTARG" >&2
+            exit 1
+            ;;
+    esac
+done
+
+shift $((OPTIND -1))
+
+if [ "$UPDATE_WEATHER" = true ]; then
+    curl "wttr.in/${CONFIG_LOCATION}"
+fi
