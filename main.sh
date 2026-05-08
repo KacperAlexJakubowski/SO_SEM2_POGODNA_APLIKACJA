3a1cb417bcc4fd41a88be82b82541e8ebdf9c5be
Kacper Jakubowski <kacper.jakubowski.my@gmail.com>
2026-05-08 04:50:12 +0200

    Initial commit

---
+++ main.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+# Author           : Kacper Jakubowski ( s208525@pg.edu.pl )
+# Created On       : 08.05.2026
+# Last Modified On : 08.05.2026 
+# Version          : 1.0
+#
+# Description      :
+#
+# Licensed under GPL (see /usr/share/common-licenses/GPL for more details
+# or contact # the Free Software Foundation for a copy)
+# 
+# Generative AI statement (keep ONE line below, delete the others):
+# * I did NOT use GenAI tools while developing this code.
+
