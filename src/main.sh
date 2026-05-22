0dc424f66b861ec4f79fa722dd3e164662f1dc78
Kacper Jakubowski <kacper.jakubowski.my@gmail.com>
2026-05-22 03:36:51 +0200

    successful weather data in json format fetch

--- src/main.sh
+++ src/main.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 # Author           : Kacper Jakubowski ( s208525@pg.edu.pl )
 # Created On       : 08.05.2026
-# Last Modified On : 08.05.2026 
+# Last Modified On : 22.05.2026 
 # Version          : 1.0
 #
 # Description      :
@@ -21,6 +21,7 @@ if [ ! -f "$CONFIG_FILE" ]; then
 fi
 source "$CONFIG_FILE"
 
+LOC=""
 UPDATE_WEATHER=false
 while getopts ":hvlu" opt; do
     case ${opt} in
@@ -36,7 +37,7 @@ while getopts ":hvlu" opt; do
             read -p "Nazwa nowej lokalizacji: " LOC
             CONFIG_LOCATION=${LOC// /+}
             sed -i "s#^CONFIG_LOCATION=.*#CONFIG_LOCATION=\"$LOC\"#" "$CONFIG_FILE"
-            echo "Zapisano nową lokalizację."
+            echo "Zapisano nową lokalizację"
             ;;
         u )
             UPDATE_WEATHER=true
@@ -51,10 +52,47 @@ done
 shift $((OPTIND -1))
 
 if [ "$UPDATE_WEATHER" = true ]; then
-    REQUEST_OPTIONS="lang=pl"    
-    BACKGROUND="../backgrounds/Partly cloudy.jpg"
-    OUTPUT="../output/output.png"
+    WEATHER_TEMP="../temp/weather.json"
+    BACKGROUNDS="../backgrounds/"
+    OUTPUT="../output/output.jpg"
 
-    # fetch information in JSON format
-    curl "wttr.in/${CONFIG_LOCATION}?format=j1" > pogoda.json
+    # fetch weather in JSON format
+    curl "wttr.in/${CONFIG_LOCATION}?format=j1" > "${WEATHER_TEMP}"
+
+    # data fetch from json
+    if [ ! -f "${WEATHER_TEMP}" ]; then
+        echo "Nie udało się pobrać informacji o aktualnej pogodzie w ${CONFIG_LOCATION}"
+        exit 1
+    fi
+
+    echo "Pomyślnie pobrano informacje o pogodzie"
+
+    TEMPERATURE=$(jq -r ".current_condition[0].temp_C" "${WEATHER_TEMP}")
+    FEEL_TEMPERATURE=$(jq -r ".current_condition[0].FeelsLikeC" "${WEATHER_TEMP}")
+    HUMIDITY=$(jq -r ".current_condition[0].humidity" "${WEATHER_TEMP}")
+    OBSV_TIME=$(jq -r ".current_condition[0].observation_time" "${WEATHER_TEMP}")
+    CODE=$(jq -r ".current_condition[0].weatherCode" "${WEATHER_TEMP}")
+    DSC=$(jq -r ".current_condition[0].weatherDesc[0].value" "${WEATHER_TEMP}")
+    ICON=$(jq -r ".current_condition[0].weatherIconUrl[0].value" "${WEATHER_TEMP}")
+    WIND_SPEED=$(jq -r ".current_condition[0].windspeedKmph" "${WEATHER_TEMP}")
+    COUNTRY=$(jq -r ".nearest_area[0].country[0].value" "${WEATHER_TEMP}")
+
+    # # data fetch test
+    # echo "${TEMPERATURE}"
+    # echo "${FEEL_TEMPERATURE}"
+    # echo "${HUMIDITY}"
+    # echo "${OBSV_TIME}"
+    # echo "${CODE}"
+    # echo "${DSC}"
+    # echo "${ICON}"
+    # echo "${WIND_SPEED}"
+    # echo "${COUNTRY}"
+
+    BACKGROUND="${BACKGROUNDS}/${CODE}.jpg"
+
+    
+
+elif [[ -z "$LOC" ]]; then
+    echo "Nie wybrano żadnej opcji"
+    exit 1
 fi
